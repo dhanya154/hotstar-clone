c56ddd0d8a5948ff120b919d59dd111ae94b31db
dhanya154 <142925184+dhanya154@users.noreply.github.com>
2023-08-29 19:16:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des: " Following the events of “Avengers: Endgame,” Sam Wilson/Falcon (Anthony Mackie) and Bucky Barnes/Winter Soldier (Sebastian Stan) team up in a global adventure that tests their abilities—and their patience—in Marvel Studios’ “The Falcon and The Winter Soldier.” The all-new series is directed by Kari Skogland; Malcolm Spellman is the head writer. ",
+    image: "images/slider-2.PNG",
+  },
+  {
+    name: "Loki",
+    des: " In Marvel Studios’ “Loki,” the mercurial villain Loki (Tom Hiddleston) resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.” Kate Herron directs and Michael Waldron is head writer. ",
+    image: "images/slider-1.PNG",
+  },
+  {
+    name: "wanda vision",
+    des: "Marvel Studios' captivating new series WandaVision which premieres in early 2021 on Disney+. Starring Elizabeth Olsen and Paul Bettany, WandaVision marks the first series from Marvel Studios streaming exclusively on Disney+. The series is a blend of classic television and the Marvel Cinematic Universe in which Wanda Maximoff and Vision—two super-powered beings living idealized suburban lives—begin to suspect that everything is not as it seems.",
+    image: "images/slider-3.PNG",
+  },
+  {
+    name: "Luca",
+    des: " Set in a beautiful seaside town on the Italian Riviera, Disney and Pixar’s original feature film “Luca” is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply-held secret: they are sea monsters from another world just below the water’s surface.",
+    image: "images/slider-5.PNG",
+  },
+  {
+    name: "Raya And The Last Dragon",
+    des: "The film follows Raya, a warrior princess who must find the fabled last dragon in order to save her divided home world from a ravenous plague. Throughout her journey, Raya traverses the various kingdoms of Kumandra, with an unusual band of misfits by her side.",
+    image: "images/slider-4.PNG",
+  },
+];
+
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+let slideIndex = 0;
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+  //create dom elements
+  let slide = document.createElement("div");
+  var imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  //attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(imgElement);
+  slide.appendChild(content);
+  carousel.appendChild(slide);
+
+  //setting up images
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  //setting elements classnames
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px) `;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+//video cards
+
+const videoCards = [...document.querySelectorAll(".video-card")];
+videoCards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+//card-sliders
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+  let containerdimensions = item.getBoundingClientRect();
+  let containerWidth = containerdimensions.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 200;
+  });
+
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 200;
+  });
+});
